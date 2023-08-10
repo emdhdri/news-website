9285e23f2af5293c01bf5a8102d03b4d34dcb94d
Emad <emdhdri@gmail.com>
2023-08-10 18:54:13 +0330

    did some modification on model.py

--- news/models.py
+++ news/models.py
@@ -9,8 +9,7 @@ class News(models.Model):
     title = models.CharField(verbose_name='title', max_length=200)
     text = models.TextField(verbose_name='news text')
     tags = ListCharField(base_field=models.CharField(max_length=TAG_MAX_LENGTH),
-                         max_length = (TAGS_MAX_COUNT * TAG_MAX_LENGTH),
-                         )
+                         max_length = (TAGS_MAX_COUNT * TAG_MAX_LENGTH))
     source = models.URLField(verbose_name='source of news url')
 
     def __str__(self):
