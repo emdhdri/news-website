0a1009a72e8d36ba6bdc9dd6f20a47cf7218a1e0
Emad <emdhdri@gmail.com>
2023-08-17 18:18:49 +0330

    create item for news data

--- news_scraper/news_scraper/items.py
+++ news_scraper/news_scraper/items.py
@@ -4,9 +4,17 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import Join, MapCompose, TakeFirst
 
 
-class NewsScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class NewsItem(scrapy.Item):
+    title = scrapy.Field(
+        output_processor=TakeFirst())
+    
+    text = scrapy.Field(
+        output_processor=Join())
+    
+    tags = scrapy.Field()
+
+    source = scrapy.Field(
+        output_processor=TakeFirst())
